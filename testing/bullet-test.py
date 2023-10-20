935becde6618e6839ade68efec98ffc89b18464d
Michael Giglia <michael.a.giglia@gmail.com>
2023-10-20 17:04:25 -0400

    crude p control

--- testing/bullet-test.py
+++ testing/bullet-test.py
@@ -4,6 +4,7 @@ import pybullet_data
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Vector3
+import os
 
 class FrictionlessMass(Node):
    def __init__(self, node_name):
@@ -25,17 +26,17 @@ physicsClient = p.connect(p.GUI)# p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 p.setGravity(0,0,-10)
 planeId = p.loadURDF("plane.urdf")
-cubeStartPos = [0,0,1]
+cubeStartPos = [0,0,0.5]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("block.urdf",cubeStartPos, cubeStartOrientation)
+boxId = p.loadURDF(os.path.join(os.path.dirname(__file__), "myblock.urdf"),cubeStartPos, cubeStartOrientation)
 rclpy.init()
 node = FrictionlessMass('mass')
 
 for i in range (10000):
    p.stepSimulation()
    time.sleep(1./240.)
-   p.applyExternalForce(boxId, -1, [0.01,0,0], [0,0,0], p.WORLD_FRAME)
    pos, rot = p.getBasePositionAndOrientation(boxId)
+   p.applyExternalForce(boxId, -1, [(1-pos[0])*1.5,0,0], [0,0,0], p.WORLD_FRAME)
    node.publish_state(pos)
 
 cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
