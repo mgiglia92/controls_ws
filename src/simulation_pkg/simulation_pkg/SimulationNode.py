5b6997a89ead2c3a4716d2d38a14d1cbd7e5288c
Michael Giglia <michael.a.giglia@gmail.com>
2023-10-31 15:05:51 -0400

    Converted Vector3 to Twist on topics, added applytorque

--- src/simulation_pkg/simulation_pkg/SimulationNode.py
+++ src/simulation_pkg/simulation_pkg/SimulationNode.py
@@ -3,21 +3,22 @@ import time
 import pybullet_data
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Twist
 from controller_interfaces.msg import State
 import os
 import traceback
 from threading import Thread
 
-class FrictionlessMass(Node):
+class FrictionlessMass1D(Node):
    def __init__(self, node_name):
       super().__init__(node_name)
-      self.input = Vector3()
+      self.input = Twist()
       self._pub = self.create_publisher(State, '/state', 10)
-      self._sub = self.create_subscription(Vector3, '/input', self.input_callback, 10)
+      self._sub = self.create_subscription(Twist, '/input', self.input_callback, 10)
       self._sub
+      self.kill = False
       
-   def input_callback(self, msg:Vector3):
+   def input_callback(self, msg:Twist):
       self.input = msg
    
    def publish_state(self, pos0): 
@@ -25,12 +26,30 @@ class FrictionlessMass(Node):
       msg.pose.position.x=pos0[0]
       self._pub.publish(msg)
 
+class FrictionlessMass2D(Node):
+   def __init__(self, node_name):
+      super().__init__(node_name)
+      self.input = Twist()
+      self._pub = self.create_publisher(State, '/state', 10)
+      self._sub = self.create_subscription(Twist, '/input', self.input_callback, 10)
+      self._sub
+      self.kill = False
+      
+   def input_callback(self, msg:Twist):
+      self.input = msg
+   
+   def publish_state(self, pos0): 
+      msg = State()
+      msg.pose.position.x=pos0[0]
+      msg.pose.position.y=pos0[1]
+      self._pub.publish(msg)
 
 def spin(node):
     try:
-        while True:
+        while not node.kill:
             rclpy.spin(node)
     except:
+        rclpy.shutdown()
         traceback.print_exc()
 
 
@@ -44,28 +63,37 @@ def main():
     cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
     boxId = p.loadURDF(os.path.join(os.path.dirname(__file__), "myblock.urdf"),cubeStartPos, cubeStartOrientation)
     if not rclpy.ok(): rclpy.init()
-    node = FrictionlessMass('mass')
+    node = FrictionlessMass1D('mass')
     pos0=[0,0,0]
     pos1=pos0
-    dt=0.01
+    dt=0.005
     p.setTimeStep(dt)
     spin_thread = Thread(target=spin, args=[node])
     spin_thread.start()
-
-    for i in range (10000):
-        p.stepSimulation()
-        time.sleep(dt)
-        pos1, rot = p.getBasePositionAndOrientation(boxId)
-        p.applyExternalForce(boxId, -1, [node.input.x, 0, 0], [0,0,0], p.WORLD_FRAME)
-        pos0 = pos1
-        node.publish_state(pos0)
-        node.get_logger().info(f"Published: {pos0}")
-
+    start = time.time_ns()
+    cur = time.time_ns() - start
+    prev = cur
+    try:
+        while True:
+            cur = time.time_ns() - start
+            if(((cur - prev)/float(1e9)) >= dt):
+                # p.stepSimulation()
+                pos1, rot = p.getBasePositionAndOrientation(boxId)
+                p.applyExternalForce(boxId, -1, [node.input.linear.x, node.input.linear.y, node.input.linear.z], [0,0,0], p.WORLD_FRAME)
+                p.applyExternalTorque(boxId, -1, [node.input.angular.x, node.input.angular.y, node.input.angular.z], p.LINK_FRAME)
+                # pos0 = pos1
+                node.publish_state(pos1)
+                node.get_logger().info(f"dt: {(cur - prev)}")
+                prev = cur
+                p.stepSimulation()
+    except KeyboardInterrupt:
+        traceback.print_exc()
 
     cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
     print(cubePos,cubeOrn)
 
     p.disconnect()
+    node.kill = True
 
 if __name__ == "__main__":
    main()
