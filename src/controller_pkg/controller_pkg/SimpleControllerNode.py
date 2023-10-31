e959b3eeb3042657e820082403fc1d23f371dc4c
Michael Giglia <michael.a.giglia@gmail.com>
2023-10-31 15:03:11 -0400

    convert Vector3 to Twist on topics

--- src/controller_pkg/controller_pkg/SimpleControllerNode.py
+++ src/controller_pkg/controller_pkg/SimpleControllerNode.py
@@ -1,23 +1,62 @@
 import rclpy
 from rclpy.node import Node
 from .ControllerInterface import PIDController_Beard, PIDControllerParameters
-from geometry_msgs.msg import Twist, Vector3
+from geometry_msgs.msg import Twist
 from controller_interfaces.msg import State
+import traceback
 
 class SimpleControllerNode(Node, PIDController_Beard):
-    
+
     def __init__(self, node_name: str, params: PIDControllerParameters):
         Node.__init__(self, node_name=node_name)
         PIDController_Beard.__init__(self, params=params)
-        self._pub = self.create_publisher(Vector3, '/input', 10)
-        self._sub = self.create_subscription(State, '/state', self.state_callback, 10)
-        print("here")
-    
+        
+        self.desired = float(1.0)
+        self.actual  = float()
+        
+        self._pub = self.create_publisher(Twist, '/input', 10)
+        self._sub_actual = self.create_subscription(State, '/state', self.state_callback, 10)
+        self._sub_desired = self.create_subscription(State, '/setpoint', self.setpoint_callback, 10)
+        self._timer = self.create_timer(params.Ts, callback=self.send_controller_output)
+        
+
     def state_callback(self, msg: State):
-        actual = msg.pose.position.x
-        u = self.calculate_controls(1, actual)
-        pub_msg = Vector3()
-        pub_msg.x = u
+        self.actual = msg.pose.position.x
+    
+    def setpoint_callback(self, msg: State):
+        self.desired = msg.pose.position.x
+
+    def send_controller_output(self):
+        u = self.calculate_controls(self.desired, self.actual)
+        pub_msg = Twist()
+        pub_msg.linear.x = u
+        self._pub.publish(pub_msg)
+
+class SimpleControllerNode2d(Node, PIDController_Beard):
+
+    def __init__(self, node_name: str, params: PIDControllerParameters):
+        Node.__init__(self, node_name=node_name)
+        PIDController_Beard.__init__(self, params=params)
+        
+        self.desired = float(1.0)
+        self.actual  = float()
+        
+        self._pub = self.create_publisher(Twist, '/input', 10)
+        self._sub_actual = self.create_subscription(State, '/state', self.state_callback, 10)
+        self._sub_desired = self.create_subscription(State, '/setpoint', self.setpoint_callback, 10)
+        self._timer = self.create_timer(params.Ts, callback=self.send_controller_output)
+        
+
+    def state_callback(self, msg: State):
+        self.actual = msg.pose.position.x
+    
+    def setpoint_callback(self, msg: State):
+        self.desired = msg.pose.position.x
+
+    def send_controller_output(self):
+        u = self.calculate_controls(self.desired, self.actual)
+        pub_msg = Twist()
+        pub_msg.linear.x = u
         self._pub.publish(pub_msg)
 
 def main():
@@ -26,6 +65,7 @@ def main():
     try:
         rclpy.spin(s)
     except:
+        traceback.print_exc()
         rclpy.shutdown()
 
 if __name__ == '__main__':
