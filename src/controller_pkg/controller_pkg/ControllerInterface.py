cade09fc2be1fac40530e686986d624f8f6d6606
Michael Giglia <michael.a.giglia@gmail.com>
2023-10-31 15:00:55 -0400

    Added vectorized version of PID Beard

--- src/controller_pkg/controller_pkg/ControllerInterface.py
+++ src/controller_pkg/controller_pkg/ControllerInterface.py
@@ -48,7 +48,8 @@ class PIDControllerParameters(ControllerParametersInterface):
     sigma: float
     flag: bool
 
-    def __init__(self, kp=1.0, ki=0.0, kd=0.0, Ts=0.01, llim=-1, ulim=1, sigma=0.01, flag=True):
+    def __init__(self, kp=float(1.0), ki=float(0.0), kd=float(0.0), Ts=float(0.01), \
+                 llim=float(-100), ulim=float(100), sigma=float(0.01), flag=True):
         self.kp = kp
         self.ki = ki
         self.kd = kd
@@ -142,6 +143,88 @@ class PIDController_Beard(ControllerInterface):
         self.y_d1 = y
         return u_sat
 
-    # Saturate the controller output
-    def saturate(self, u):
-        return np.clip(u, self.llim, self.ulim)
+ 
+
+class PIDController_Beard_Vectorized(ControllerInterface):
+
+    params: list[PIDControllerParameters]
+
+    def __init__(self, params: list[PIDControllerParameters]):
+        self.kp         = np.array([], dtype=np.float32) # Proportional control gain
+        self.ki         = np.array([], dtype=np.float32) # Integral control gain
+        self.kd         = np.array([], dtype=np.float32) # Derivative control gain
+        self.llim       = np.array([], dtype=np.float32) # The output saturates at this limit
+        self.ulim       = np.array([], dtype=np.float32)
+        self.sigma      = np.array([], dtype=np.float32) # dirty derivative bandwidth is 1/sigma
+        self.beta       = np.array([], dtype=np.float32)
+        self.Ts         = np.array([], dtype=np.float32) # sample rate
+        self.flag       = np.array([], dtype=np.float32)
+        self.y_dot      = np.array([], dtype=np.float32)
+        self.y_d1       = np.array([], dtype=np.float32)
+        self.error_dot  = np.array([], dtype=np.float32)
+        self.error_d1   = np.array([], dtype=np.float32)
+        self.integrator = np.array([], dtype=np.float32)
+
+        for p in params:
+            self.kp = np.append(self.kp, p.kp)
+            self.ki = np.append(self.ki, p.ki)
+            self.kd = np.append(self.kd, p.kd)
+            self.llim = np.append(self.llim, p.llim)
+            self.ulim = np.append(self.ulim, p.ulim)
+            self.sigma = np.append(self.sigma, p.sigma)
+            self.beta = np.append(self.beta, p.beta)
+            self.Ts = np.append(self.Ts, p.Ts)
+            self.flag = np.append(self.flag, p.flag)
+
+            # Initialize previous sample data vals
+            self.y_dot = np.append(self.y_dot, 0.0)
+            self.y_d1 = np.append(self.y_d1, 0.0)
+            self.error_dot = np.append(self.error_dot, 0.0)
+            self.error_d1 = np.append(self.error_d1, 0.0)
+            self.integrator = np.append(self.integrator, 0.0)
+
+    def calculate_controls(self, y_r: np.ndarray, y: np.ndarray):
+        assert (type(y_r) == np.ndarray and type(y) == np.ndarray), "Make sure to input a list"
+        assert (len(y_r) == len(y) == len(self.kp)),                "Input length mismatch to controller"
+        # Compute the current error
+        error = y_r - y
+        # integrate error using trapazoidal rule
+        self.integrator = self.integrator \
+        + (self.Ts/2) * (error + self.error_d1)
+        # PID Control
+        if self.flag is True:
+            # differentiate error
+            self.error_dot = self.beta * self.error_dot \
+            + (1-self.beta)/self.Ts * (error - self.error_d1)
+            # PID control
+            u_unsat = self.kp*error \
+            + self.ki*self.integrator \
+            + self.kd*self.error_dot
+        else:
+            # differentiate y
+            self.y_dot = self.beta * self.y_dot \
+            + (1-self.beta)/self.Ts * (y - self.y_d1)
+            # PID control
+            u_unsat = self.kp*error \
+            + self.ki*self.integrator \
+            - self.kd*self.y_dot
+            # return saturated control signal
+        u_sat = self.saturate(u_unsat)
+
+        # integrator anti - windup
+        for i, ki in enumerate(self.ki):
+            if ki != 0.0:
+                self.integrator[i] = self.integrator[i] \
+                + 1.0 / ki * (u_sat - u_unsat)
+                # update delayed variables
+                self.error_d1[i] = error[i]
+                self.y_d1[i] = y[i]
+        return u_sat
+
+
+
+if __name__ == "__main__":
+    l1 = PIDControllerParameters()
+    l2 = PIDControllerParameters()
+    p = PIDController_Beard_Vectorized([l1,l2])
+    p.calculate_controls(np.array([10,100]),np.array([0,0]))
